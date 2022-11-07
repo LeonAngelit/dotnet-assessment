3688e1f7dc0aad3aa714e7498fa1021f63a051d4
LeonAngelit <81989897+LeonAngelit@users.noreply.github.com>
2022-11-07 13:04:34 +0100

    data results

--- assesment-front/src/Components/FarewellComponent/FarewellComponent.js
+++ assesment-front/src/Components/FarewellComponent/FarewellComponent.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 function FarewellComponent() {
-  return <div>Data sent correctly</div>;
+	const results = [
+		{ name: 'DATA', afinity: 50 },
+		{ name: 'LOGISTICS', afinity: 80 },
+		{ name: 'IDENTITY_VALIDATION', afinity: 90 },
+		{ name: 'FIDELIZATION', afinity: 60 },
+		{ name: 'PAYMENTS', afinity: 100 },
+	];
+	return (
+		<div className='results-container'>
+			<h2>Results: </h2>
+
+			<BarChart
+				width={800}
+				height={400}
+				data={results}>
+				<XAxis dataKey='name' />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Bar
+					dataKey='afinity'
+					barSize={50}
+					fill='#8884d8'
+				/>
+			</BarChart>
+		</div>
+	);
 }
 
 export default FarewellComponent;
