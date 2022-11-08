670465d2d135e3e763ebde61bf7801b32854204a
LeonAngelit <81989897+LeonAngelit@users.noreply.github.com>
2022-11-08 14:12:43 +0100

    error messages

--- assesment-front/src/Components/FarewellComponent/FarewellComponent.js
+++ assesment-front/src/Components/FarewellComponent/FarewellComponent.js
@@ -1,82 +1,89 @@
-import axios from "axios";
-import { React, useContext } from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from "recharts";
-import AppContext from "../../store/AppContext";
+import axios from 'axios';
+import { React, useContext } from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import AppContext from '../../store/AppContext';
 
 function FarewellComponent(props) {
-  const currentAppContext = useContext(AppContext);
-  const temporalAnswer = { ...currentAppContext.answer };
+	const currentAppContext = useContext(AppContext);
+	const temporalAnswer = { ...currentAppContext.answer };
 
-  const results = [
-    {
-      name: "DATA",
-      afinity: Math.round(
-        (temporalAnswer.data.reduce((a, b) => a + b) /
-          temporalAnswer.data.length) *
-          100
-      ),
-    },
-    {
-      name: "LOGISTICS",
-      afinity: Math.round(
-        (temporalAnswer.logistics.reduce((a, b) => a + b) /
-          temporalAnswer.logistics.length) *
-          100
-      ),
-    },
-    {
-      name: "IDENTITY_VALIDATION",
-      afinity: Math.round(
-        (temporalAnswer.identity.reduce((a, b) => a + b) /
-          temporalAnswer.identity.length) *
-          100
-      ),
-    },
-    {
-      name: "FIDELIZATION",
-      afinity: Math.round(
-        (temporalAnswer.fidelization.reduce((a, b) => a + b) /
-          temporalAnswer.fidelization.length) *
-          100
-      ),
-    },
-    {
-      name: "PAYMENTS",
-      afinity: Math.round(
-        (temporalAnswer.payments.reduce((a, b) => a + b) /
-          temporalAnswer.payments.length) *
-          100
-      ),
-    },
-  ];
+	const results = [
+		{
+			name: 'DATA',
+			afinity: Math.round(
+				(temporalAnswer.data.reduce((a, b) => a + b) /
+					temporalAnswer.data.length) *
+					100
+			),
+		},
+		{
+			name: 'LOGISTICS',
+			afinity: Math.round(
+				(temporalAnswer.logistics.reduce((a, b) => a + b) /
+					temporalAnswer.logistics.length) *
+					100
+			),
+		},
+		{
+			name: 'IDENTITY_VALIDATION',
+			afinity: Math.round(
+				(temporalAnswer.identity.reduce((a, b) => a + b) /
+					temporalAnswer.identity.length) *
+					100
+			),
+		},
+		{
+			name: 'FIDELIZATION',
+			afinity: Math.round(
+				(temporalAnswer.fidelization.reduce((a, b) => a + b) /
+					temporalAnswer.fidelization.length) *
+					100
+			),
+		},
+		{
+			name: 'PAYMENTS',
+			afinity: Math.round(
+				(temporalAnswer.payments.reduce((a, b) => a + b) /
+					temporalAnswer.payments.length) *
+					100
+			),
+		},
+	];
 
-  axios
-    .post(
-      "https://localhost:7010/api/User",
-      { email: currentAppContext.userEmail, answers: JSON.stringify(results) },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-    .then((response) => {
-    console.log(response)
-    });
+	axios
+		.post(
+			'https://localhost:7010/api/User',
+			{ email: currentAppContext.userEmail, answers: JSON.stringify(results) },
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		)
+		.then((response) => {
+			window.localStorage.removeItem('app-context');
+		});
 
-  return (
-    <div className="results-container">
-      <h2>Results: </h2>
+	return (
+		<div className='results-container'>
+			<h2>Results: </h2>
 
-      <BarChart width={800} height={400} data={results}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="afinity" barSize={50} fill={props.color} />
-      </BarChart>
-    </div>
-  );
+			<BarChart
+				width={800}
+				height={400}
+				data={results}>
+				<XAxis dataKey='name' />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Bar
+					dataKey='afinity'
+					barSize={50}
+					fill={props.color}
+				/>
+			</BarChart>
+		</div>
+	);
 }
 
 export default FarewellComponent;
--- assesment-front/src/Components/InitialComponent/LoginComponent.js
+++ assesment-front/src/Components/InitialComponent/LoginComponent.js
@@ -1,79 +1,137 @@
-import { React, useContext, useRef, useEffect } from "react";
-import AppContext from "../../store/AppContext";
-import "./Login.component.css";
-import axios from "axios";
+import { React, useContext, useRef, useEffect } from 'react';
+import AppContext from '../../store/AppContext';
+import './Login.component.css';
+import axios from 'axios';
 
 function LoginComponent() {
-  const emailRef = useRef(null);
-  const emailTwoRef = useRef(null);
-  const currentAppContext = useContext(AppContext);
-  const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+	const emailRef = useRef(null);
+	const emailTwoRef = useRef(null);
+	const currentAppContext = useContext(AppContext);
+	const validateEmail = (email) => {
+		return String(email)
+			.toLowerCase()
+			.match(
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+			);
+	};
 
-  useEffect(() => {
-    if (currentAppContext.questions.length == 0) {
-      getQuestions();
-    }
-  }, []);
+	useEffect(() => {
+		if (currentAppContext.questions.length == 0) {
+			getQuestions();
+		}
+	}, []);
 
-  function getQuestions() {
-    axios
-      .get("https://localhost:7010/api/Question", {
-        headers: {
-          Accept: "application/json",
-        },
-      })
-      .then((response) => {
-        currentAppContext.setQuestions(response.data);
-      });
-  }
+	function getQuestions() {
+		axios
+			.get('https://localhost:7010/api/Question', {
+				headers: {
+					Accept: 'application/json',
+				},
+			})
+			.then((response) => {
+				currentAppContext.setQuestions(response.data);
+			});
+	}
 
-  function handleClick(e) {
-    e.preventDefault();
+	function checkUser(user) {
+		axios
+			.get(`https://localhost:7010/api/User/${user}`, {
+				headers: {
+					Accept: 'application/json',
+				},
+			})
+			.then((response) => {
+				return response.data.length == 0;
+			});
+	}
 
-    if (
-      emailRef.current.value === emailTwoRef.current.value &&
-      emailRef.current.value != "" &&
-      emailTwoRef.current.value != "" &&
-      validateEmail(emailRef.current.value)
-    ) {
-      currentAppContext.setUserEmail(emailRef.current.value);
-      currentAppContext.setStep(++currentAppContext.step);
-    } else {
-      document
-        .getElementsByClassName("errorSpan")[0]
-        .classList.remove("invisible");
-    }
-  }
+	function validateEmpty() {
+		if (emailRef.current.value != '' && emailTwoRef.current.value != '') {
+			return true;
+		}
+		return false;
+	}
 
-  return (
-    <div className="login-form">
-      <form>
-        <div className="input-container">
-          <label htmlFor="email">Email: </label>
-          <input type="email" placeholder="Email" name="email" ref={emailRef} />
-        </div>
-        <div className="input-container">
-          <label htmlFor="email">Repeat email: </label>
-          <input
-            type="email"
-            placeholder="Email"
-            name="repeatEmail"
-            ref={emailTwoRef}
-          />
-        </div>
-        <div className="submit-container">
-          <input type="submit" defaultValue="Accept" onClick={handleClick} />
-        </div>
-      </form>
-      <span className="errorSpan invisible">The emails don't match</span>
-    </div>
-  );
+	function validateMatch(params) {
+		if (emailRef.current.value === emailTwoRef.current.value) {
+			return true;
+		}
+
+		return false;
+	}
+
+	function handleClick(e) {
+		e.preventDefault();
+
+		if (!validateEmpty()) {
+			document.getElementById('empty').classList.remove('invisible');
+
+			return false;
+		}
+
+		if (!validateMatch()) {
+			document.getElementById('empty').classList.add('invisible');
+			document.getElementById('unmatch').classList.remove('invisible');
+
+			return false;
+		}
+
+		if (!checkUser(emailRef.current.value)) {
+			document.getElementById('unmatch').classList.add('invisible');
+			document.getElementById('registered').classList.remove('invisible');
+			return false;
+		}
+
+		currentAppContext.serUserEmail(emailRef.current.value);
+		currentAppContext.setSteip(++currentAppContext.step);
+	}
+
+	return (
+		<div className='login-form'>
+			<form>
+				<div className='input-container'>
+					<label htmlFor='email'>Email: </label>
+					<input
+						type='email'
+						placeholder='Email'
+						name='email'
+						ref={emailRef}
+					/>
+				</div>
+				<div className='input-container'>
+					<label htmlFor='email'>Repeat email: </label>
+					<input
+						type='email'
+						placeholder='Email'
+						name='repeatEmail'
+						ref={emailTwoRef}
+					/>
+				</div>
+				<div className='submit-container'>
+					<input
+						type='submit'
+						defaultValue='Accept'
+						onClick={handleClick}
+					/>
+				</div>
+			</form>
+			<span
+				className='errorSpan invisible'
+				id='unmatch'>
+				The emails don't match
+			</span>
+			<span
+				className='errorSpan invisible'
+				id='empty'>
+				The emails field cannot be empty
+			</span>
+			<span
+				className='errorSpan invisible'
+				id='registered'>
+				Email already registered
+			</span>
+		</div>
+	);
 }
 
 export default LoginComponent;
